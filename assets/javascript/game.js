8bfaa35186fb2d8c8e023d25e56159e1f40cf65d
Yadira Soto <lunar13heart@hotmail.com>
2019-05-30 21:49:08 -0400

    added functions & console

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,30 @@
 var victory = 0;
 var word = 0;
-var remaining = ;
+var remaining = 10;
 var guessed = [];
 
-var computerPicks =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z",];
-var computerGuess = '';
+var userPicks =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z",];
+var userGuess = '';
+
+function userSelect() {
+    userGuess =
+        userPicks[(Math.random() * userPicks.length)]
+}
+
+function updatedScores() {
+    document.querySelector("#victory").innerHTML = victory;
+    document.querySelector("#word").innerHTML = word;
+    document.querySelector("#remaining").innerHTML = remaining;
+    document.querySelector("#guessed").innerHTML = guessed;
+
+}
+
+function resetScores() {
+    word = 0;
+    remaining = 10;
+    guessed = [];
+}
+
+userSelect();
+
+console.log(userGuess);
