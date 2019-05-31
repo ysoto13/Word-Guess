fec73ec3d36327fb01de0e6d4d08ac1b3a1f3efe
Yadira Soto <lunar13heart@hotmail.com>
2019-05-31 15:14:59 -0400

    added a comment

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,9 +7,18 @@ var guessed = [];
 var userPicks =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z",];
 var userGuess = '';
 
+// var avocado = 7;
+// var cake = 4;
+// var coconut = 7;
+// var cookies = 7;
+// var gummies = 7;
+// var macaroon = 8;
+// var mango = 5;
+// var watermelon = 10;
+
 function userSelect() {
     userGuess =
-        word[(Math.random() * word.length)]
+        userPicks[(Math.random() * userPicks.length)]
 }
 
 function updatedScores() {
@@ -27,20 +36,30 @@ function resetScores() {
 
 userSelect();
 
+console.log(userGuess)
+
 document.onkeyup = function(event) {
     remaining--;
 
-    var guessingWord = event.key.toLowerCase();
-    guessed.push(guessingWord);
+    var userGuess = event.key.toLowerCase();
+    guessed.push(userGuess);
 
     updatedScores();
 
-    if ((guesssed === "avocado") || (guesssed === "cake") || (guesssed === "coconut") || (guesssed === "cookies") || (guesssed === "gummies") || (guesssed === "macaroon") || (guesssed === "mango") || (guesssed === "atermelon"))
+    if ((guesssed === "avocado") || (guesssed === "cake") || (guesssed === "coconut") || (guesssed === "cookies") || (guesssed === "gummies") || (guesssed === "macaroon") || (guesssed === "mango") || (guesssed === "watermelon"))
      {
-        victory++
+    victory++
         updatedScores()
         resetScores()
         userSelect()
+    } else (userGuess === userSelect)
+
     }
 
-}
+    if (remaining ===0) {
+        updatedScores()
+        resetScores()
+    }
+
+
+// I got stuck with my javscript and didn't know what next steps I had to take. I also had a bad time managing my time between school and work so I was only able to work on it for only one day. I didn't want to turn in nothing so I turned in something. 
