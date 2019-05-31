3c6174ac41aa6b3da24104ce8366d3077b9c3259
Yadira Soto <lunar13heart@hotmail.com>
2019-05-30 21:34:06 -0400

    attempting js now

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+var victory = 0;
+var word = 0;
+var remaining = ;
+var guessed = [];
+
+var computerPicks =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z",];
+var computerGuess = '';
